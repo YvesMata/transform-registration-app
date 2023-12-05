fd3baf98638ddb2b389aeb675342f2b83d022f6e
YvesMata <yvus.mata@up.phinma.edu.ph>
2023-12-05 10:35:53 +0800

    Navbar update + Success page

--- js/main.js
+++ js/main.js
@@ -2,23 +2,29 @@
 
 const navBar = document.getElementById("nav-bar");
 const navBarButton = document.getElementById("nav-bar-button");
+const navOverlay = document.getElementById("nav-overlay");
 
-let buttonStateMenu = 0;
+let buttonStateMenu = 1;
 
 navBarButton.addEventListener('click', () => {
 	if (buttonStateMenu === 0) {
 		navBar.classList.add('side-bar-hide');
 		buttonStateMenu = 1;
+		navOverlay.style.display = 'none';
 		console.log(buttonStateMenu);
 	}
 	else {
 		navBar.classList.remove('side-bar-hide');
 		buttonStateMenu = 0;
+		navOverlay.style.display = 'block';
 		console.log(buttonStateMenu);
 	}
 });
 
+navOverlay.addEventListener('click', () => {
+	navBar.classList.add('side-bar-hide');
+	buttonStateMenu = 1;
+	navOverlay.style.display = 'none';
+});
+
 //======================FORM=================================
-
-
-
